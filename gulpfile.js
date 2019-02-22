e8ce0290c3ba0958a63c7375253e3d333bd9d0a3
Nastya Stifutina <stifutina@gmail.com>
2019-02-22 19:36:24 +0200

    autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const gulpUglify = require('gulp-uglify');
 /* CSS */
 const gulpScssLint = require('gulp-scss-lint');
 const gulpSass = require('gulp-sass');
+const gulpAutoprefixer = require('gulp-autoprefixer');
 
 function eslint() {
     return gulp.src('src/js/*.js')
@@ -47,12 +48,21 @@ function scssCss() {
         .pipe(gulp.dest('dist/style'));
 }
 
+function autoprefixer() {
+    return gulp.src('dist/style/*.css')
+        .pipe(gulpAutoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false,
+        }))
+        .pipe(gulp.dest('dist/style/'));
+}
 
 gulp.task('eslint', eslint);
 gulp.task('babel', babel);
 gulp.task('uglify', uglify);
 gulp.task('scsslint', scssLint);
 gulp.task('scsscss', scssCss);
+gulp.task('autoprefixer', autoprefixer);
 
 
-gulp.task('default', gulp.series('eslint', 'babel', 'uglify', 'scsslint', 'scsscss'));
+gulp.task('default', gulp.series('eslint', 'babel', 'uglify', 'scsslint', 'scsscss', 'autoprefixer'));
